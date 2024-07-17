64f17ae4695c319940acdd10d6ac9f8f8acabd8b
sandro <alex.andreevskiy@gmail.com>
2024-07-17 21:14:22 +0200

    small fixes

--- app/Models/Parcel.php
+++ app/Models/Parcel.php
@@ -2,20 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Parcel extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
-//        'first_name',
-//        'middle_name',
-//        'last_name',
-//        'phone',
-//        'email',
-//        'address',
         'user_id',
         'width',
         'height',
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -24,34 +23,10 @@ class User extends Authenticatable
         'email',
         'phone',
         'address',
-//        'password',
     ];
 
     public function parcels()
     {
         return $this->hasMany(Parcel::class);
     }
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-//    protected $hidden = [
-//        'password',
-//        'remember_token',
-//    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-//    protected function casts(): array
-//    {
-//        return [
-//            'email_verified_at' => 'datetime',
-//            'password' => 'hashed',
-//        ];
-//    }
 }
